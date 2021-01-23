257f0a7a17ac0383945cb5a868a99aaa32d3ce35
Jas Haria <jasharia1998@gmail.com>
2021-01-23 18:17:56 +0530

    removing button from frontend

--- void-backend/service/util_service.py
+++ void-backend/service/util_service.py
@@ -19,7 +19,7 @@ def get_new_session():
 def get_driver():
     options = Options()
     options.add_argument("--headless")
-    driver = webdriver.Chrome(ChromeDriverManager().install())#, options=options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     #driver = webdriver.Chrome(r"C:/Users/jasha/.wdm/drivers/chromedriver/win32/86.0.4240.22/chromedriver.exe")#, options=options)
     return driver
 
